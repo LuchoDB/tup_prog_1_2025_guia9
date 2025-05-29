423886c1af033f363ac723fc33277279566efd8a
LuchoDB <lucianodiazbertozzi@gmail.com>
2025-05-29 18:55:04 -0300

    Ejercicio1 - Iterativos

---
+++ Guia9/Ejercicio1/Ejercicio1-Iterativo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            int primero=0, segundo=1;
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.Write(primero + " ");
+                int aux = primero + segundo;
+                primero = segundo;
+                segundo = aux;
+            }
+        }
+    }
+}
